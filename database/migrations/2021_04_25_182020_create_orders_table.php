7f99bffaec482e2158a7515622b1308303fcf288
DESKTOP-CSH72C6\lviet <lviet456@gmail.com>
2023-03-03 13:24:33 +0700

    Feat(Migration) add foreignkey address_id to order table

--- database/migrations/2021_04_25_182020_create_orders_table.php
+++ database/migrations/2021_04_25_182020_create_orders_table.php
@@ -16,6 +16,7 @@ class CreateOrdersTable extends Migration
         Schema::create('order', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id', false, true);
+            $table->foreignId('address_id')->constrained('addresses');
             $table->timestamp('order_date');
             $table->decimal('order_amount', 8, 2, true);
         });
