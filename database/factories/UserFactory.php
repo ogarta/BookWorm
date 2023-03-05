0682cda98af23ff2cc822df43815a698a7a20db6
DESKTOP-CSH72C6\lviet <lviet456@gmail.com>
2023-03-05 23:34:19 +0700

    Fead(Factory) factory user add field phone

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,8 @@ class UserFactory extends Factory
         return [
             'first_name' => $this->faker->firstName(),
             'last_name' => $this->faker->name(),
+            // fake phone number have 10 digits
+            'phone_number' => $this->faker->numberBetween(1000000000, 9999999999),
             'email' => $this->faker->unique()->safeEmail(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ];
