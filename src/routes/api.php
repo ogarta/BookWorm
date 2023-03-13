071179ac7b0b5c3481d514be849c89d7b8c25288
DESKTOP-CSH72C6\lviet <lviet456@gmail.com>
2023-03-13 14:47:26 +0700

    Feat: build docker composer and docker file for project

---
+++ src/routes/api.php
@@ -0,0 +1,85 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\BookController;
+use App\Http\Controllers\Api\ShopController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\AuthorController;
+use App\Http\Controllers\Api\ReviewController;
+use App\Http\Controllers\Api\AddressController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ReciverController;
+use App\Models\Book;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// User Auth
+Route::prefix('auth')->group(function () {
+	Route::post('login', [AuthController::class, 'login'])
+	->name('login');
+	Route::middleware('auth:sanctum')->get('logout', [AuthController::class, 'logout'])
+	->name('logout');
+	Route::post('sign-up', [AuthController::class, 'signUp']);
+	Route::middleware('auth:sanctum')->get('/', [AuthController::class, 'user']);
+	Route::middleware('auth:sanctum')->post('/edit-pass', [AuthController::class, 'editPassword']);
+});
+
+// User
+Route::prefix('user')->middleware('auth:sanctum')->group(function() {
+	Route::apiResource('address', AddressController::class);
+});
+
+// Home Page
+Route::prefix('home')->name('home.')->group(function () {
+	Route::get('/books/top-discount',[BookController::class,'getTopDiscount'])->name('top-discount');
+	Route::get('/books/top-recommend',[BookController::class,'getTopRecommend'])->name('top-recommend');
+	Route::get('/books/top-popular',[BookController::class,'getTopPopular'])->name('top-popular');
+});
+
+//Shop Page
+Route::prefix('shop')->group(function () {
+	Route::apiResource('author', AuthorController::class)->only('index');
+	Route::apiResource('category', CategoryController::class)->only('index');
+	Route::get('/{sort?}{order?}{category_id?}{author_id?}{num_rating?}{num_item?}', [ShopController::class, 'filterAndSortBookBy'])
+	->name('filter_sort_book');
+});
+
+//Product Page
+Route::prefix('product')->group(function () {
+	Route::post('/book/list',[BookController::class,'getListBook']);
+	Route::apiResource('book', BookController::class)->only('show');
+	Route::prefix('/review')->group(function () {
+		Route::get('/rating/{id?}',[ReviewController::class,'getDetailRating']);
+		Route::get('/{id?}{sort?}{rating_star?}{num_item?}',[ReviewController::class,'getDetailReview']);
+		Route::post('/create',[ReviewController::class,'createReview']);
+	});
+});
+
+// Cart Page
+Route::middleware('auth:sanctum')->prefix('cart')->group(function () {
+	Route::apiResource('order', OrderController::class);
+});
+
+Route::post('test', function (Request $request) {
+	return Book::all();
+});
+
+
+
+
+
+
+
